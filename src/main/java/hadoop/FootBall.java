c9531d5bff7b164d504677c5c76e91aa05060511
Denis Yakovenko <maximka1945@gmail.com>
2017-11-09 18:59:33 +0200

    Initial commit

---
+++ src/main/java/hadoop/FootBall.java
@@ -0,0 +1,96 @@
+package hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Assert;
+
+import java.io.IOException;
+
+public class FootBall {
+
+    private static class FootballMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            if (value.toString().equals("name,club,age,position,position_cat,market_value,page_views,fpl_value,fpl_sel,fpl_points,region,nationality,new_foreign,age_cat,club_id,big_club,new_signing"))
+                return;
+            String[] fieldValues = value.toString().split(",");
+            word.set(fieldValues[11]);
+            context.write(word, one);
+        }
+    }
+
+    private static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    private static class KeyComparator extends WritableComparator {
+        public KeyComparator() {
+            super(Text.class, true);
+        }
+
+        @Override
+        public int compare(WritableComparable a, WritableComparable b) {
+            return super.compare(a, b);
+        }
+    }
+
+    private static class KeyPartitioner extends Partitioner<Text, IntWritable> {
+        @Override
+        public int getPartition(Text key, IntWritable value, int numReduceTasks) {
+            return key.toString().matches("[a-kA-K](.*)") ? 0 : 1;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem hdfs = FileSystem.get(conf);
+        //Path inDir = new Path("input"/*args[0]*/);
+        Path outDir = new Path("output"/*args[1]*/);
+        hdfs.delete(outDir, true);
+        if (hdfs.exists(outDir))
+            return;
+        Job job = Job.getInstance(conf, "football nationality count");
+        job.setJarByClass(FootBall.class);
+        job.setMapperClass(FootballMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setPartitionerClass(KeyPartitioner.class);
+        job.setNumReduceTasks(2);
+        job.setSortComparatorClass(KeyComparator.class);
+        //FileInputFormat.addInputPath(job, inDir);
+        FileInputFormat.addInputPath(job, new Path("epldata_final.csv"));
+        FileOutputFormat.setOutputPath(job, outDir);
+        Assert.assertTrue(job.waitForCompletion(true));
+        FileStatus[] outputFiles = FileSystem.get(job.getConfiguration()).listStatus(outDir);
+        for (FileStatus file : outputFiles) {
+            System.out.println(file.getPath() + " " + file.getLen());
+        }
+    }
+}
---
+++ src/main/java/hadoop/FootBallNationalityCount.java
@@ -0,0 +1,193 @@
+package hadoop;
+
+import org.junit.Assert;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueLineRecordReader;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * футбольний датасет
+ * задача - посортувати по громадянству(колонка "nationality") футболістів
+ */
+
+public class FootBallNationalityCount extends Job {
+
+    private void createInput(Configuration conf, Path inDir) throws IOException {
+        Path infile = new Path(inDir, "text.txt");
+        OutputStream os = FileSystem.get(conf).create(infile);
+        Writer wr = new OutputStreamWriter(os);
+        /*
+          Consider a word count input file, where the first line of the file
+          contains the words to use in the count.
+          COUNT=dog,cat
+          My dog is not a cat
+          my dog is not a giraffe
+          my dog is my freind!
+          The outputs of a word count for this job would be:
+          dog,3
+          cat,1
+          In order to implement this sort of wordount, we need non splittable inputs, that is,
+          inputs which process headers independently.
+         */
+        wr.write("dog,cat\n");
+        wr.write("My dog is not a cat\n");
+        wr.write("My dog is not a cat\n");
+        wr.write("my dog is not a giraffe\n");
+        wr.write("my dog is my freind!\n");
+        wr.close();
+        System.out.println("DONE WRITING " + infile);
+    }
+
+    private static class WordCountMapper extends Mapper<Text, Text, Text, IntWritable> {
+
+        private static final IntWritable ONE = new IntWritable(1);
+
+        protected void map(Text offset, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer tok = new StringTokenizer(value.toString(), " ");
+            while (tok.hasMoreTokens()) {
+                Text word = new Text(tok.nextToken());
+                context.write(word, ONE);
+            }
+        }
+    }
+
+    private static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    private static class HeaderInputFormat extends FileInputFormat<Text, Text> {
+        Text header;
+        Set<String> wordsToCount = new HashSet<>();
+
+        /**
+         * We dont split files when files have headers.  We need to read the
+         * header in the init method, and then use that meta data in the header
+         * for counting all words in the file.
+         */
+        @Override
+        protected boolean isSplitable(JobContext context, Path file) {
+            return false;
+        }
+
+        @Override
+        public RecordReader<Text, Text> createRecordReader(InputSplit arg0, TaskAttemptContext arg1) throws IOException, InterruptedException {
+            return new KeyValueLineRecordReader(arg1.getConfiguration()) {
+                @Override
+                public void initialize(InputSplit genericSplit, TaskAttemptContext context) throws IOException {
+                    super.initialize(genericSplit, context);
+                    /*
+                     * Read the first line.
+                     */
+                    super.nextKeyValue();
+                    header = super.getCurrentKey();
+                    System.out.println("header = " + header);
+                    if (header.toString().length() == 0)
+                        throw new RuntimeException("No header :(");
+                    String[] words = header.toString().split(",");
+                    for (String s : words) {
+                        wordsToCount.add(s);
+                        System.out.println("COUNTING " + s);
+                    }
+                    System.out.println("\nCOUNT ::: " + wordsToCount);
+                }
+
+                /**
+                 * In this case, we'll just return the same thing
+                 * for both the key and value.  A little strange but..
+                 * its a good way to demonstrate that the RecordReader
+                 * decouples us from the default implementation of key/value
+                 * parsing... which is the point of this post anyways :)
+                 */
+                @Override
+                public Text getCurrentKey() {
+                    // TODO Auto-generated method stub
+                    return this.getCurrentValue();
+                }
+
+                /**
+                 * Starts at line 2.  First line is the header.
+                 */
+                @Override
+                public Text getCurrentValue() {
+                    String raw = super.getCurrentKey().toString();
+                    String[] fields = raw.split(" ");
+                    String filteredLine = "";
+                    /*
+                     * Use the metadata in the header
+                     * to filter out strings which we're not interested in.
+                     */
+                    for (String f : fields) {
+                        if (wordsToCount.contains(f)) {
+                            filteredLine += f + " ";
+                        }
+                    }
+                    System.out.println("RECORD READER FILTERED LINE USING HEADER META DATA: " + filteredLine);
+                    return new Text(filteredLine);
+                }
+
+            };
+        }
+    }
+
+    public FootBallNationalityCount(Configuration conf) throws IOException {
+        try {
+            Path inDir = new Path("inputSJWH");
+            Path outDir = new Path("outputSJWH");
+            FileSystem.get(this.getConfiguration()).delete(inDir, true);
+            FileSystem.get(this.getConfiguration()).delete(outDir, true);
+            createInput(this.getConfiguration(), inDir);
+            setJobName("MapReduce example for scientific data with headers");
+            setOutputKeyClass(Text.class);
+            setOutputValueClass(IntWritable.class);
+            setInputFormatClass(HeaderInputFormat.class);
+            setMapperClass(WordCountMapper.class);
+            setReducerClass(IntSumReducer.class);
+            FileInputFormat.addInputPath(this, inDir);
+            FileOutputFormat.setOutputPath(this, outDir);
+            Assert.assertTrue(this.waitForCompletion(true));
+            // Check the output is as expected
+            FileStatus[] outputFiles = FileSystem.get(this.getConfiguration())
+                    .listStatus(outDir);
+            for (FileStatus file : outputFiles) {
+                System.out.println(file.getPath() + " " + file.getLen());
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            new FootBallNationalityCount(new Configuration());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
